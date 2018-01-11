5ca83d7f4cff63bd407d6fc439eec3308472c20a
lmm915815 <tanliqing2010@163.com>
2018-01-11 13:49:44 +0800

    update

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ wget -c https://github.com/lmm915815/tab-python/blob/master/tab.py -O /tmp
 
 /bin/cp -f /tmp/tab.py /usr/lib/python2.7/ 
 
-echo "export PATHSTARTUP=/usr/lib/python2.7/tab.py"
+echo "export PYTHONSTARTUP=/usr/lib/python2.7/tab.py" >> ~/.bashrc 
 
