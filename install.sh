f904ee420e23aa314c5986068a288015e078035b
lmm915815 <tanliqing2010@163.com>
2018-01-11 11:44:17 +0800

    install auto

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -c https://github.com/lmm915815/tab-python/blob/master/tab.py -O /tmp 
+
+/bin/cp -f /tmp/tab.py /usr/lib/python2.7/ 
+
+echo "export PATHSTARTUP=/usr/lib/python2.7/tab.py"
+
